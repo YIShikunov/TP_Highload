af9eda28f18269c0a50ff0e6f379b4488ddbee23
timujin <newtimujin@gmail.com>
2015-10-25 14:18:03 -0300

    Stub for sending files

--- echo.py
+++ echo.py
@@ -1,10 +1,33 @@
 import eventlet
+import os
+from eventlet.green import os 
+from time import gmtime, strftime
+
+def RespondWithTestFile(client):
+	f = open("1.txt", 'r')
+
+	http_response = 'HTTP/1.1 200 OK\r\n'
+	http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
+	http_response += 'Content-Length: {0}\r\n'.format(os.stat("1.txt").st_size)
+	http_response += 'Content-Type: text/plain\r\n'
+	http_response += '\r\n'
+	client.sendall(http_response.encode())
+
+	offset = 0
+	while True:
+		sent = os.sendfile(client.fileno(), f.fileno(), offset, 4096)
+		if sent <= 0:
+			break
+		offset += sent
+	f.close()
+
 
 def handle(client):
-    while True:
-        c = client.recv(1)
-        if not c: break
-        client.sendall(c)
+    #while True:
+        #c = client.recv(1)
+        #if not c: break
+    #    client.sendall(c)
+    RespondWithTestFile(client)
 
 server = eventlet.listen(('0.0.0.0', 8080))
 pool = eventlet.GreenPool(10000)
