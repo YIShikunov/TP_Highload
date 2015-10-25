bfc41ebdb4564c820403df612448eba1d106f7ff
Yury Shikunov <YIShikunov@users.noreply.github.com>
2015-10-25 17:35:53 +0000

    Tinkering around

--- echo.py
+++ echo.py
@@ -1,15 +1,17 @@
 import eventlet
-import os
 from eventlet.green import os 
+from eventlet.green import socket
 from time import gmtime, strftime
 
 def RespondWithTestFile(client):
-	f = open("1.txt", 'r')
-
+	print('{date}: Client Connected'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
+	#f = open("1.txt", 'r')
+	
 	http_response = 'HTTP/1.1 200 OK\r\n'
 	http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
 	http_response += 'Content-Length: {0}\r\n'.format(os.stat("1.txt").st_size)
 	http_response += 'Content-Type: text/plain\r\n'
+	http_response += 'Content-Encoding: utf-8\r\n'
 	http_response += '\r\n'
 	client.sendall(http_response.encode())
 
@@ -20,7 +22,9 @@ def RespondWithTestFile(client):
 			break
 		offset += sent
 	f.close()
-
+	client.shutdown(socket.SHUT_RDWR)
+	client.close()
+	print('{date}: Connection closed'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
 
 def handle(client):
     #while True:
