01faca73971c1ca2f321d04f4daf94b0f95afa3d
Yury Shikunov <YIShikunov@users.noreply.github.com>
2015-10-27 12:04:37 +0000

    another bit of work

--- echo.py
+++ echo.py
@@ -10,9 +10,9 @@ def RespondWithTestFile(client):
 	
 	http_response = 'HTTP/1.1 200 OK\r\n'
 	http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
+	http_response += 'Server: TPHW\r\n'
 	http_response += 'Content-Length: {0}\r\n'.format(os.stat("1.txt").st_size)
 	http_response += 'Content-Type: text/plain\r\n'
-	http_response += 'Content-Encoding: utf-8\r\n'
 	http_response += '\r\n'
 	client.sendall(http_response.encode())
 
@@ -23,18 +23,21 @@ def RespondWithTestFile(client):
 			break
 		offset += sent
 	f.close()
-	client.shutdown(socket.SHUT_RDWR)
-	client.close()
-	print('{date}: Connection closed'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
+#	client.shutdown(socket.SHUT_RDWR)
+#	client.close()
+#	print('{date}: Connection closed'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
 
 
 def handleS(client):
 	print("Client connected")
 	s = client.recv(10000)
 	headers = str(s).split("\\r\\n")
+	request = headers[0].split(' ')
+	
 	if "GET" in headers[0]:
 		print(headers)
-	client.sendall(s)
+		RespondWithTestFile(client)
+#	client.sendall(s)
 	client.shutdown(socket.SHUT_RDWR)
 	client.close()
 
