08f646b2ffcf5462531766f3d34b45fd70037d42
Yury <YIShikunov@gmail.com>
2015-10-28 16:54:41 +0300

    Some stuff looks ready

--- echo.py
+++ echo.py
@@ -1,86 +1,105 @@
-import eventlet
+﻿import eventlet
 from eventlet.green import os 
 from eventlet.green import socket
 from eventlet.green.time import gmtime, strftime
 from eventlet.green import urllib
 import dicts
+from generateDirectoryIndex import generateDirectoryIndex
 
 def RespondNotSupported(client):
-	http_response = 'HTTP/1.1 ' + dicts.responseCodes['405'] + '\r\n'
-	http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
-	http_response += 'Server: TPHW\r\n'
-	http_response += '\r\n'
-	client.sendall(http_response)
+    http_response = 'HTTP/1.1 ' + dicts.responseCodes['405'] + '\r\n'
+    http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
+    http_response += 'Server: TPHW\r\n'
+    http_response += '\r\n'
+    client.sendall(http_response)
 
-def RespondFile(client, file):
-	path = urllib.unquote(file)
-	f = None
-	if (os.path.isfile(path)):
-		try:
-			f = open(path)
-		except PermissionError:
-			print("Permission error on " + path)
-			Respond404(client)
-		except IOError:
-			print("File not found on " + path)
-			Respond404(client)
-		####
-	elif (os.path.isdir(path)):
-		try:
-			f = open(os.path.join(path, 'index.html')
-		except PermissionError:
-			print("Permission error on " + path)
-			Respond404(client)
-		except IOError:
-			print("File not found on " + path)
-			f = GenerateDirectoryIndex()
-	
+
+def Respond404(client):
+    f = open("404.html", 'r')
+    http_response = 'HTTP/1.1 ' + dicts.responseCodes['404'] + '\r\n'
+    http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
+    http_response += 'Server: TPHW\r\n'
+    http_response += 'Content-Length: {0}\r\n'.format(os.stat("404.html").st_size)
+    http_response += 'Content-Type: {0}\r\n'.format(dicts.contentTypes['html'])
+    http_response += '\r\n'
+    client.sendall(http_response.encode())
+
+    offset = 0
+    while True:
+        sent = os.sendfile(client.fileno(), f.fileno(), offset, 4096)
+        if sent <= 0:
+            break
+        offset += sent
+    f.close()
+
+
+def RespondHead(client, file):
+    path = urllib.unquote(file)
+    f = None
+    if (os.path.isfile(path)):
+        try:
+            f = open(path, 'r')
+            http_response = 'HTTP/1.1 200 OK\r\n'
+            http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
+            http_response += 'Server: TPHW\r\n'
+            http_response += 'Content-Length: {0}\r\n'.format(os.stat(path).st_size)
+            http_response += 'Content-Type: {0}\r\n'.format(dicts.contentTypes.get(os.path.splittext(path)[1], 'application/octet-stream'))
+            http_response += '\r\n'
+            client.sendall(http_response.encode())
+        except PermissionError:
+            print("Permission error on " + path)
+            Respond404(client)
+        except IOError:
+            print("File not found on " + path)
+            Respond404(client)
+    elif (os.path.isdir(path)):
+        try:
+            f = open(os.path.join(path, 'index.html'), 'r')
+        except PermissionError:
+            print("Permission error on " + path)
+            Respond404(client)
+        except IOError:
+            print("File not found on " + path)
+            f = generateDirectoryIndex(path)
+    return f
+    
 #TODO
 
-def RespondWithTestFile(client):
-	print('{date}: Client Connected'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
-	f = open("1.txt", 'r')
-	
-	http_response = 'HTTP/1.1 200 OK\r\n'
-	http_response += 'Date: {date}\r\n'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime()))
-	http_response += 'Server: TPHW\r\n'
-	http_response += 'Content-Length: {0}\r\n'.format(os.stat("1.txt").st_size)
-	http_response += 'Content-Type: text/plain\r\n'
-	http_response += '\r\n'
-	client.sendall(http_response.encode())
-
-	offset = 0
-	while True:
-		sent = os.sendfile(client.fileno(), f.fileno(), offset, 4096)
-		if sent <= 0:
-			break
-		offset += sent
-	f.close()
-#	client.shutdown(socket.SHUT_RDWR)
-#	client.close()
-#	print('{date}: Connection closed'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
+def RespondWithFile(client, file):
+    if (file is str):
+        client.sendall(file.encode())
+    else:
+        offset = 0
+        while True:
+            sent = os.sendfile(client.fileno(), f.fileno(), offset, 4096)
+            if sent <= 0:
+                break
+            offset += sent
+        f.close()
+#   client.shutdown(socket.SHUT_RDWR)
+#   client.close()
 
 
-def handleS(client):
-	print("Client connected")
-	s = client.recv(10000)
-	headers = str(s).split("\\r\\n")
-	request = headers[0].split(' ')
-	if request[0] not in discts.supportedRequests:
-		RespondNotSupported(client)
-	else:
-		RespondHead(client, request[1])
-	if "GET" in headers[0]:
-		print(headers)
-		RespondWithTestFile(client)
-#	client.sendall(s)
-	client.shutdown(socket.SHUT_RDWR)
-	client.close()
+def handle(client):
+    print('{date}: Client Connected'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
+    s = client.recv(10000)
+    headers = str(s).split("\\r\\n")
+    request = headers[0].split(' ')
+    file = None
+    if request[0] not in discts.supportedRequests:
+        RespondNotSupported(client)
+    else:
+        file = RespondHead(client, request[1])
+    if (("GET" in headers[0]) and (file is not None)):
+        RespondWithFile(client, file)
+    client.shutdown(socket.SHUT_RDWR)
+    client.close()
+    print('{date}: Connection closed'.format(date=strftime("%a, %d %b %Y %X GMT", gmtime())))
 
 server = eventlet.listen(('0.0.0.0', 8080))
 pool = eventlet.GreenPool(10000)
 while True:
     new_sock, address = server.accept()
-    pool.spawn_n(handleS, new_sock)
+    pool.spawn_n(handle, new_sock)
 
 
